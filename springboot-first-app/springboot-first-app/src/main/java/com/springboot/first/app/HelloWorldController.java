130dff616ae0e57aa5eabbbe6267efcd36630d0a
ronith <ronithrajkopperla@gmail.com>
2022-01-25 12:27:57 +0530

    first commit

---
+++ springboot-first-app/springboot-first-app/src/main/java/com/springboot/first/app/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.springboot.first.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	// GET HTTP Method
+	// http://localhost:8080/hello-world
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello";
+	}
+
+}
